fba22492dd6f306b60ae009118b526fda1b3d158
Mary Kang <mary.kang09@gmail.com>
2020-03-07 21:45:08 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,12 +1,18 @@
 # require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
-  # code goes here
+def load_library(file_path)
+
+hash
 end
+
+
 
 def get_japanese_emoticon
   # code goes here
 end
+
 
 def get_english_meaning
   # code goes here
