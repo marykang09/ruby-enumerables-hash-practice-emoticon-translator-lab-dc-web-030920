5143ae49e8f25063ae35df86dae5157f91c120d9
Mary Kang <mary.kang09@gmail.com>
2020-03-07 22:31:03 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,36 @@
 # require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
-  # code goes here
+def load_library(file_path)
+  hash = {"get_meaning" => {}, "get_emoticon" => {} }
+  emoticons = YAML.load_file(file_path)
+  emoticons.each do |meaning, data|
+    english, japanese = data
+    hash["get_emoticon"][english] = japanese
+    hash["get_meaning"][japanese] = meaning
+  end
+  hash
 end
 
-def get_japanese_emoticon
-  # code goes here
+
+def get_japanese_emoticon(file_path, emoticon)
+  library = load_library(file_path)
+  result = library["get_emoticon"][emoticon]
+  if result
+    result
+  else
+    "Sorry, that emoticon was not found"
+  end
 end
 
-def get_english_meaning
-  # code goes here
+
+def get_english_meaning(file_path, emoticon)
+  library = load_library(file_path)
+  result = library["get_meaning"][emoticon]
+  if result
+    result
+  else
+    "Sorry, that emoticon was not found"
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require_relative "../lib/translator.rb"
 
 RSpec.configure do |config|
