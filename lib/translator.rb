3f466d7670a4ae91ea77f1c4c08c3e4305f5a315
Mary Kang <mary.kang09@gmail.com>
2020-03-07 22:16:52 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -10,7 +10,8 @@ hahs = {
 emoticons = YAML.load_file(file_path)
 emoticons.each do |meaning,data|
   english, japanese = data
-  "get"
+  "get_emoticon"[english] = japanese
+  "get_meaning"[japanese] = 
   
 end
 end
